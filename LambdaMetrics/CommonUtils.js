ac6b0558a573994b0199436e1abdfac2bb4df162
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-05-15 10:49:14 +0530

    API endpoints to get lambda list and individual lambda info

--- LambdaMetrics/CommonUtils.js
+++ LambdaMetrics/CommonUtils.js
@@ -1,16 +1,23 @@
-function generateResponse(payload, statusCode = 200, headers = {}) {
+module.exports = {
+
+    generateResponse: function (payload, statusCode = 200, headers = {}) {
+        return generateGenericResponse(payload, statusCode, headers);
+    },
+
+    generateSuccessResponse: function (payload) {
+        return generateGenericResponse(payload, 200, { 'access-control-allow-origin': '*' });
+    },
+
+    generateFailureResponse: function (payload) {
+        return generateGenericResponse(payload, 500, { 'access-control-allow-origin': '*' });
+    }
+}
+
+function generateGenericResponse(payload, statusCode = 200, headers = {}) {
     return {
         "isBase64Encoded": 1,
         "statusCode": statusCode,
         "headers": headers,
         "body": JSON.stringify(payload)
     };
-}
-
-function generateSuccessResponse(payload) {
-    return generateResponse(payload, 200, {'access-control-allow-origin': '*'});
-}
-
-function generateFailureResponse(payload) {
-    return generateResponse(payload, 500, {'access-control-allow-origin': '*'});
 }
--- LambdaMetrics/FunctionListHandler.js
+++ LambdaMetrics/FunctionListHandler.js
@@ -1,22 +1,75 @@
+const commonUtils = require('./CommonUtils');
 let AWS = require('aws-sdk');
-let commonUtils = require('./CommonUtils');
+const lambda = new AWS.Lambda();
 
-exports.handler = function(event, context, callback) {
+const LAMBDA_ARN_QPARAM = 'lambdaArn';
+
+exports.handler = function (event, context, callback) {
+
+	let qParams = event.queryStringParameters;
+	let qParamsKeys = Object.keys(qParams || {});
+	console.log(qParamsKeys);
+
+	if (qParamsKeys.indexOf(LAMBDA_ARN_QPARAM) >= 0) {
+		getLambdaInfo(callback, qParams[LAMBDA_ARN_QPARAM]);
+	} else {
+		listLambdas(callback, qParams);
+	}
+}
+
+function listLambdas(callback, qParams) {
 
 	var params = {
 		FunctionVersion: 'ALL',
-		// Marker: 'STRING_VALUE',
 		// MasterRegion: 'STRING_VALUE',
-		MaxItems: 100
+		MaxItems: qParams.maxLimit
 	};
-	lambda.listFunctions(params, function (err, data) {
-		if (err) { 
+	if (qParams.nextMarker) {
+		params['Marker'] = qParams.nextMarker;
+	}
+
+	lambda.listFunctions(params).promise()
+		.then(data => {
+			let nextMarker = data.NextMarker;
+			let functions = data.Functions;
+
+			let summarizedFunctions = [];
+			if (functions) {
+				summarizedFunctions = functions.map(func => {
+					return {
+						"FunctionName": func.FunctionName,
+						"FunctionArn": func.FunctionArn,
+						"Runtime": func.Runtime,
+						"Description": func.Description,
+						"Timeout": func.Timeout,
+						"MemorySize": func.MemorySize
+					}
+				});
+			}
+			callback(null, commonUtils.generateSuccessResponse({
+				NextMarker: nextMarker,
+				Functions: summarizedFunctions
+			}));
+		})
+		.catch(err => {
 			console.log(err, err.stack);
 			callback(null, commonUtils.generateFailureResponse(err));
-		}
-		else {
+		});
+}
+
+function getLambdaInfo(callback, lambdaArn) {
+
+	var params = {
+		FunctionName: lambdaArn
+		// Qualifier: 'STRING_VALUE'
+	};
+	lambda.getFunction(params).promise()
+		.then(data => {
 			console.log(data);
 			callback(null, commonUtils.generateSuccessResponse(data));
-		}
-	});
-}
+		})
+		.catch(err => {
+			console.log(err, err.stack);
+			callback(null, commonUtils.generateFailureResponse(err));
+		});
+};
